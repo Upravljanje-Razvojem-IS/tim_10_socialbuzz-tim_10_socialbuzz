45f7654fd1c16b919f684acc413178819ebfc3e7
nViolet <radibratovic.n@gmail.com>
2021-05-26 16:55:54 +0200

    removed webapplication.target

---
+++ TheSocialBazNeda/Backup/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
@@ -0,0 +1,7 @@
+namespace TheSocialBazNeda.Areas.HelpPage.ModelDescriptions
+{
+    public class CollectionModelDescription : ModelDescription
+    {
+        public ModelDescription ElementDescription { get; set; }
+    }
+}
---
+++ TheSocialBazNeda/Backup/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace TheSocialBazNeda.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
